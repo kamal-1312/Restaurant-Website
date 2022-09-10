4de6a450b98eb78a78a3eb641453c0d3133a2190
Kamal Kant <103095890+kamal-1312@users.noreply.github.com>
2022-09-10 21:14:38 +0530

    Added src folder including all the components required for the Website

---
+++ src/component/menuApi.js
@@ -0,0 +1,86 @@
+const Menu = [
+  {
+    id: 1,
+    image: "../images/toast.jpeg",
+    name: "FRENCH TOAST",
+    category: "Breakfast",
+    price: "40₹",
+    description:
+      "Crème Brulée Batter, Berries, Crème Anglaise, Maple Syrup . ",
+  },
+
+  {
+    id: 2,
+    image: "../images/sandwitch.jpeg",
+    name: "OG BREAKFAST",
+    category: "Happy-Hour",
+    price: "50₹",
+    description:
+      "Two Eggs, Toast, Breakfast Potatoes, Choice of: Bacon, Sausage or Avocado ",
+  },
+  {
+    id: 3,
+    image: "../images/salad.jpeg",
+    name: "SALAD",
+    category: "Breakfast",
+    price: "40₹",
+    description:
+      "Romaine, Red Onion, Cucumber, Tomato, Croutons, Parmesan, Choice of Dressing. ",
+  },
+  {
+    id: 4,
+    image: "../images/chilli.jpeg",
+    name: "Chilli Paneer 13 / Chilli Chicken 11",
+    category: "Lunch",
+    price: "150₹",
+    description:
+      "is a spicy, deep-fried boneless chicken marinated in spices ",
+  },
+  {
+    id: 5,
+    image: "../images/mojito.jpeg",
+    name: "Mojito",
+    category: "Happy-Hour",
+    price: "80₹",
+    description:
+      "The fresh, fragrant mint accentuates and contrasts the sweet sugar can undertones of the rum ",
+  },
+  {
+    id: 6,
+    image: "../images/Rogangosh.jpeg",
+    name: "Rogangosh",
+    category: "Dinner",
+    price: "180₹",
+    description:
+      "Choice of protein cooked with cinnamon, turmeric, and coriander.  ",
+  },
+  {
+    id: 7,
+    image: "../images/carrot.jpeg",
+    name: "Carrot Halwa",
+    category: "Dinner",
+    price: "60₹",
+    description:
+      "Carrot Halwa (with cashews and raisins) ",
+  },
+  {
+    id: 8,
+    image: "../images/malai.jpeg",
+    name: "Malai Kofta",
+    category: "Lunch",
+    price: "160₹",
+    description:
+      "Cheese and vegetable dumplings simmered in a creamy sauce with cashews, raisins, and coconut . ",
+  },
+  {
+    id: 9,
+    image: "../images/chai.jpg",
+    name: "Masala Chai",
+    category: "Happy-Hour",
+    price: "20₹",
+    description:
+      "Tea flavored with cardamom, cloves and cinnamon with or without milk ",
+  },
+];
+
+export default Menu;
